0b302cf20409b86393395db1d5a353b796b74714
coco-desktop <535346881@qq.com>
2019-03-11 20:48:57 +0800

    顶部的滚动监听

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(window).scroll(function(){
+        //navbar状态的切换
+        var st = $(window).scrollTop()
+        if(st > 250) {
+            $('.navbar').addClass('navbar-fixed-top')
+        }else{
+           $('.navbar').removeClass('navbar-fixed-top')
+        }
+    })
+})
